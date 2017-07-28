797b3713b2fb6dd26c76af49ff2059e6f0cbb653
kisters <kisters@it>
2017-07-28 04:37:23 -0300

    Permission denied Who Cares

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,10 +3,10 @@
 echo "magento"
 du -sh ./src
 echo "clean ./var ./setup ./dev ./src/pub/static ./src/setup-mage"
-rm -rf ./src/var/* ./src/setup/* ./src/dev/* ./src/pub/static/* ./src/setup-mage
+sudo rm -rf ./src/var/* ./src/setup/* ./src/dev/* ./src/pub/static/* ./src/setup-mage
 du -sh ./src
 
 echo "preparar ZIP \n--------------------------------------------"
 
-zip -rq magento.zip ./src
+sudo zip -rq magento.zip ./src
 du -sh ./magento.zip
