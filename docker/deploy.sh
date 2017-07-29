635a5ba892021161fd2190e694f6a39910124087
kisters <kisters@it>
2017-07-29 02:28:48 -0300

    workando

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,7 +3,10 @@
 echo "magento"
 du -sh ./src
 echo "clean ./var ./setup ./dev ./src/pub/static ./src/setup-mage"
+ls -la ./src/var/
+chmod -R ./src/var ./src/pub/static ./src/setup-mage
 rm -rf ./src/var/* ./src/pub/static/* ./src/setup-mage
+ls -la ./src/var/
 du -sh ./src
 
 echo "preparar ZIP"
