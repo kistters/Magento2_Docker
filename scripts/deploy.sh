75329b62d1196d1382798dbcc01e48ad37aa1577
kisters <kisters@it>
2017-08-01 00:30:26 -0300

    start continuos deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,14 +4,14 @@ eval $(aws ecr get-login --region us-west-2)
 
 
 push_nginx(){
-	docker tag magento2devops_web:latest 125590874533.dkr.ecr.us-west-2.amazonaws.com/mage2-devops:latest
-	docker push 125590874533.dkr.ecr.us-west-2.amazonaws.com/mage2-devops:latest
+	docker tag magento2devops_web:latest 125590874533.dkr.ecr.us-west-2.amazonaws.com/mage2-devops-web:latest
+	docker push 125590874533.dkr.ecr.us-west-2.amazonaws.com/mage2-devops-web:latest
 }
 
 push_phpfpm(){
-	docker tag magento2devops_phpfpm:latest 125590874533.dkr.ecr.us-west-2.amazonaws.com/mage2-devops:latest
-	docker push 125590874533.dkr.ecr.us-west-2.amazonaws.com/mage2-devops:latest
+	docker tag magento2devops_phpfpm:latest 125590874533.dkr.ecr.us-west-2.amazonaws.com/mage2-devops-phpfpm:latest
+	docker push 125590874533.dkr.ecr.us-west-2.amazonaws.com/mage2-devops-phpfpm:latest
 }
 
-#push_nginx
+push_nginx
 push_phpfpm
