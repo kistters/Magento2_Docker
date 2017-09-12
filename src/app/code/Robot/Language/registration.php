7cc7605083574358b03d20aef4fc1f2f1f69a26f
Rafael Ventura <rafael.silva@yandeh.com.br>
2017-09-12 15:56:01 -0300

    Unit Test in Layer Company Module

---
+++ src/app/code/Robot/Language/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Robot_Language',
+    __DIR__
+);
---
+++ src/app/code/Robot/Moviment/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Robot_Moviment',
+    __DIR__
+);
